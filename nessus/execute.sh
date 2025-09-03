0d4ee380a9aa2e94a89556ecab8f04c0d33e423d
DedyCated inc <dedycated.inc@gmail.com>
2025-09-04 00:08:57 +0700

    nessus scripts

--- nessus/execute.sh
+++ nessus/execute.sh
@@ -6,6 +6,12 @@ FILENAME0=`basename $0`; FILE=$(echo $FILENAME0 | cut -d '.' -f 1)
 echo [$BASEDIR00] [$CURRDIR00] [$FILENAME0] [$FILE]
 echo "......."
 cd $BASEDIR00
-docker-compose up -d
+if [ $1 == "down" ]; then
+  docker-compose down
+elif [ $1 == "up" ]; then
+  docker-compose up -d
+else
+  echo "Invalid argument $1"
+fi
 ~/commands/docker/ps/all.sh
 exit 0
