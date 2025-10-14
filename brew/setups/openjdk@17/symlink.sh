d21d3725dedfd677c17cd4c9edbcb5912ff127f2
DedyCated inc <dedycated.inc@gmail.com>
2025-10-14 20:40:47 +0700

    brew setups openjdk@17

---
+++ brew/setups/openjdk@17/symlink.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo -e "\nBase Directory and Filename..."
+BASEDIR00="$( cd "$( dirname "$0" )" && pwd )"
+CURRDIR00=${BASEDIR00##*/}
+FILENAME0=`basename $0`; FILE=$(echo $FILENAME0 | cut -d '.' -f 1)
+echo [$BASEDIR00] [$CURRDIR00] [$FILENAME0] [$FILE]
+echo "......."
+cd $BASEDIR00
+sudo ln -sfn /usr/local/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
+ls -al /Library/Java/JavaVirtualMachines/openjdk-17.jdk
+exit 0
