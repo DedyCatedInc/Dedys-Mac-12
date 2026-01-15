04143ed4936a9423a9ca2d828c0c08978008fd37
DedyCated inc <dedycated.inc@gmail.com>
2026-01-15 10:43:59 +0700

    nessus updates

--- nessus/export.sh
+++ nessus/export.sh
@@ -6,19 +6,6 @@ FILENAME0=`basename $0`; FILE=$(echo $FILENAME0 | cut -d '.' -f 1)
 echo [$BASEDIR00] [$CURRDIR00] [$FILENAME0] [$FILE]
 echo "......."
 cd $BASEDIR00
-case "$1" in
-  down)
-    docker-compose down
-    ;;
-  up)
-    docker-compose up -d
-    ;;
-  "")
-    echo "Invalid argument (missing command). Use: up | down"
-    ;;
-  *)
-    echo "Invalid argument $1"
-    ;;
-esac
-~/commands/docker/ps/all.sh
+docker commit nessus01-dedy-irawan-dedycated-com nessus01-dedy-irawan-dedycated-com:snapshot
+docker save -o nessus01-dedy-irawan-dedycated-com_snapshot.tar nessus01-dedy-irawan-dedycated-com:snapshot
 exit 0
