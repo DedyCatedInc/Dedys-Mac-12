f73299df0d9e024a50d030679483505d372f6c87
DedyCated inc <dedycated.inc@gmail.com>
2026-02-15 15:21:17 +0700

    brew updates

--- brew/upgrade.sh
+++ brew/upgrade.sh
@@ -6,6 +6,7 @@ FILENAME0=`basename $0`; FILE=$(echo $FILENAME0 | cut -d '.' -f 1)
 echo [$BASEDIR00] [$CURRDIR00] [$FILENAME0] [$FILE]
 echo "......."
 cd $BASEDIR00
+sudo xcode-select -r
 USER=$(whoami)
 chown -R $USER /usr/local/Cellar
 brew ${FILE}
