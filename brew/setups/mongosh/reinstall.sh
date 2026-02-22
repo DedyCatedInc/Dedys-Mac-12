368668002ce13919778ccb8a71002e79978c88c7
DedyCated inc <dedycated.inc@gmail.com>
2026-02-22 18:53:19 +0700

    brew setups mongosh

---
+++ brew/setups/mongosh/reinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo -e "\nBase Directory and Filename..."
+BASEDIR00="$( cd "$( dirname "$0" )" && pwd )"
+CURRDIR00=${BASEDIR00##*/}
+FILENAME0=`basename $0`; FILE=$(echo $FILENAME0 | cut -d '.' -f 1)
+echo [$BASEDIR00] [$CURRDIR00] [$FILENAME0] [$FILE]
+echo "......."
+cd $BASEDIR00
+chmod -R 777 /usr/local/Cellar
+brew update
+brew $FILE $CURRDIR00
+brew cleanup
+chmod -R 777 /usr/local/Cellar
+exit 0
